2d2cb682064d8d40cbe16e363a1db7ec54f54b90
MIGUEL ANGEL CHILLON VAZQUEZ <zc01331@tc2.tech>
2025-05-06 16:03:00 +0200

    V5

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import time
 import datetime
 import os
