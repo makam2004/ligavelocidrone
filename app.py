b1436cb903377d123a31933080d9f6d6b3b997b5
MIGUEL ANGEL CHILLON VAZQUEZ <zc01331@tc2.tech>
2025-05-07 19:13:10 +0200

    Versión 23 para subir a Render

--- app.py
+++ app.py
@@ -55,6 +55,10 @@ def obtener_resultados(url, jugadores_objetivos):
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-zygote")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-software-rasterizer")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
